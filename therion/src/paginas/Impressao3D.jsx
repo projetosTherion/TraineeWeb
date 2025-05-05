b5ff2bc051db91bcadf74b1087ec3673a9e99419
Lucas Peluca <lucaspeluca@alunos.utfpr.edu.br>
2025-05-05 15:19:08 -0300

    - Adiciona estilo centralizado à section - Alteração do título e subtítulo (Fonte, tamanho, cor...) - Aplica justificação no subtítulo - Ajusta o espaçamento do subtítulo para melhor legibilidade

--- therion/src/paginas/Impressao3D.jsx
+++ therion/src/paginas/Impressao3D.jsx
@@ -1,11 +1,14 @@
 function Impressao3D() {
-    return (
-      <section>
-        <h1>Impressão 3D</h1>
-        <p>Serviços de impressão 3D, catálogo e informações para compras.</p>
-      </section>
-    );
-  }
-  
-  export default Impressao3D;
-  
+  return (
+    <section style={{ textAlign: 'center' }}>
+      <h1 style={{ color: '#FF5991', fontFamily: 'League Spartan', fontSize: '2.0rem', marginTop: '0.5rem', fontWeight: 'bold' }}>
+        DÊ VIDA ÀS SUAS IDEIAS COM IMPRESSÃO 3D SOB MEDIDA
+      </h1>
+      <p style={{ textAlign: 'justify', color: '#000000', fontFamily: 'League Spartan', maxWidth: '400px', fontSize: '1.25rem', marginTop: '0.25rem', marginLeft: 'auto', marginRight: 'auto' }}>
+        Transforme conceitos em realidade com agilidade, precisão e alto nível de detalhamento. Oferecemos serviços de impressão 3D e prototipagem personalizados, ideais para projetos de engenharia, design, educação e inovação tecnológica.
+      </p>
+    </section>
+  );
+}
+
+export default Impressao3D;
