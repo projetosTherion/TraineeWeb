0960c0b3013975feeeb08959760f6c1299f460b5
Hanntterene <lohanna.s.monteiro@gmail.com>
2025-05-06 20:57:51 -0300

    Teste Header

--- therion/src/components/Header.jsx
+++ therion/src/components/Header.jsx
@@ -2,14 +2,23 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header style={{ background: '#ff1493', padding: '10px', color: '#fff' }}>
+    <header style={{ background: ' #FF5991', padding: '10px', color: '#fff' }}>
       <nav>
         <ul style={{ listStyle: 'none', display: 'flex', gap: '15px' }}>
-          <li><Link to="/" style={{ color: '#fff' }}>Home</Link></li>
+          <li>
+            <Link to="/" style={{ color: '#fff' }}>Home</Link>
+          </li>
           <li><Link to="/sobre" style={{ color: '#fff' }}>Sobre Nós</Link></li>
           <li><Link to="/projetos" style={{ color: '#fff' }}>Projetos</Link></li>
           <li><Link to="/impressao3d" style={{ color: '#fff' }}>Impressão 3D</Link></li>
           <li><Link to="/contato" style={{ color: '#fff' }}>Contato</Link></li>
+        </ul>
+      </nav>
+      <nav>
+        <ul style={{listStyle: 'none', display: 'flex', gap: '15px' }}>
+          <li>
+            <Link to="/" style={{ color: '#fff' }}> Cadastro </Link> 
+          </li>
         </ul>
       </nav>
     </header>
