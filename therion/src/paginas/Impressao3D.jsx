560e0603156e1446646cc93fd3d61593b34a7761
Sefora-Davanso <seforadavanso@gmail.com>
2025-05-10 18:06:24 -0300

    Adicione os imports das imagens no arquivo Impressao3D

--- therion/src/paginas/Impressao3D.jsx
+++ therion/src/paginas/Impressao3D.jsx
@@ -6,6 +6,12 @@ import trofeuImg from '../assets/trofeu.jpeg';
 import impressaoVideo from '../assets/impressao.mp4';
 import panterionFilamento from '../assets/panteraFilamento.png';
 
+import ideiaImg from '../assets/ideia.jpg';
+import orcamentoImg from '../assets/orcamento.jpg';
+import cuboImg from '../assets/cubo-3d.jpg';
+import modelagemImg from '../assets/modelagem-3d.jpg';
+import aprovadoImg from '../assets/aprovado.jpg';
+import entregaImg from '../assets/entrega-rapida.jpg';
 
 
 function Impressao3D() {
@@ -36,7 +42,7 @@ function Impressao3D() {
         </p>
 
         <div className={styles.cards}>
-          <div className={styles.card2}>
+          <div className={styles.card}>
             <img src={chaveiroImg} alt="Chaveiro" className={styles.imagem} />
             <h3 className={styles.nome}>Chaveiro</h3>
             <p className={styles.descricao}>
@@ -44,7 +50,7 @@ function Impressao3D() {
             </p>
           </div>
 
-          <div className={styles.card2}>
+          <div className={styles.card}>
             <img src={trofeuImg} alt="Troféu" className={styles.imagem} />
             <h3 className={styles.nome}>Troféu</h3>
             <p className={styles.descricao}>
@@ -52,11 +58,11 @@ function Impressao3D() {
             </p>
           </div>
 
-          <div className={styles.card2}>
+          <div className={styles.card}>
             <img src={suporteImg} alt="Suporte" className={styles.imagem} />
             <h3 className={styles.nome}>Suporte</h3>
             <p className={styles.descricao}>
-              Um suporte para Alexa é um acessório projetado para acomodar seu dispositivo oferecendo estabilidade e um visual mais personalizado
+              Um suporte para Alexa é um acessório projetado para acomodar seu dispositivo oferecendo estabilidade e um visual mais personalizado.
             </p>
           </div>
         </div>
@@ -75,47 +81,48 @@ function Impressao3D() {
       </section>
 
       {/* SEÇÃO 3 : METODOLOGIA */}
-<section className={styles.metodologiaSection}>
-  <h2 className={styles.titulo}>Metodologia</h2>
-  <p className={styles.subtitulo}>
-    Confira nosso portfólio e conheça as peças que já desenvolvemos com impressão 3D!
-  </p>
+      <section className={styles.metodologiaSection}>
+        <h2 className={styles.titulo}>Metodologia</h2>
+        <p className={styles.subtitulo}>
+          Confira nosso portfólio e conheça as peças que já desenvolvemos com impressão 3D!
+        </p>
 
-  <div className={styles.metodologiaGrid}>
-    <div className={styles.metodologiaCard}>
-      <img src={ideiaImg} alt="Ícone ideia" className={styles.metodologiaIcon} />
-      <p>O primeiro passo é ouvir você. Coletamos todas as informações sobre o que deseja criar. Seja um objeto funcional, decorativo ou protótipo — projeto 100% personalizado.</p>
-    </div>
+        <div className={styles.metodologiaGrid}>
+          <div className={styles.metodologiaCard}>
+            <img src={ideiaImg} alt="Ícone ideia" className={styles.metodologiaIcon} />
+            <p>O primeiro passo é ouvir você. Coletamos todas as informações sobre o que deseja criar. Seja um objeto funcional, decorativo ou protótipo — projeto 100% personalizado.</p>
+          </div>
 
-    <div className={styles.metodologiaCard}>
-      <img src={cuboImg} alt="Ícone engrenagem" className={styles.metodologiaIcon} />
-      <p>Avaliamos se o projeto é viável tecnicamente. Consideramos fatores como tipo de material, escala de impressão, nível de detalhe e limitações da tecnologia.</p>
-    </div>
+          <div className={styles.metodologiaCard}>
+            <img src={cuboImg} alt="Ícone engrenagem" className={styles.metodologiaIcon} />
+            <p>Avaliamos se o projeto é viável tecnicamente. Consideramos fatores como tipo de material, escala de impressão, nível de detalhe e limitações da tecnologia.</p>
+          </div>
 
-    <div className={styles.metodologiaCard}>
-      <img src={modelagemImg} alt="Ícone modelagem" className={styles.metodologiaIcon} />
-      <p>Nossa equipe irá desenvolver ou modelar o arquivo 3D. Criamos ou adaptamos o arquivo otimizado para garantir compatibilidade e qualidade.</p>
-    </div>
+          <div className={styles.metodologiaCard}>
+            <img src={modelagemImg} alt="Ícone modelagem" className={styles.metodologiaIcon} />
+            <p>Nossa equipe irá desenvolver ou modelar o arquivo 3D. Criamos ou adaptamos o arquivo otimizado para garantir compatibilidade e qualidade.</p>
+          </div>
 
-    <div className={styles.metodologiaCard}>
-      <img src={orcamentoImg} alt="Ícone orçamento" className={styles.metodologiaIcon} />
-      <p>Enviamos uma proposta com orçamento. Tipo de material, tempo de impressão, acabamento e envio. Números combinados e acordados previamente.</p>
-    </div>
+          <div className={styles.metodologiaCard}>
+            <img src={orcamentoImg} alt="Ícone orçamento" className={styles.metodologiaIcon} />
+            <p>Enviamos uma proposta com orçamento. Tipo de material, tempo de impressão, acabamento e envio. Números combinados e acordados previamente.</p>
+          </div>
 
-    <div className={styles.metodologiaCard}>
-      <img src={aprovadoImg} alt="Ícone aprovação" className={styles.metodologiaIcon} />
-      <p>Após sua aprovação, iniciamos o processo. Monitoramos o processo de impressão e mantemos você informado sobre cada etapa.</p>
-    </div>
+          <div className={styles.metodologiaCard}>
+            <img src={aprovadoImg} alt="Ícone aprovação" className={styles.metodologiaIcon} />
+            <p>Após sua aprovação, iniciamos o processo. Monitoramos o processo de impressão e mantemos você informado sobre cada etapa.</p>
+          </div>
 
-    <div className={styles.metodologiaCard}>
-      <img src={entregaImg} alt="Ícone entrega" className={styles.metodologiaIcon} />
-      <p>Enviamos a peça com toda proteção necessária. Garantimos qualidade no transporte e incluímos instruções para melhor uso e cuidado futuro.</p>
-    </div>
-  </div>
-</section>
+          <div className={styles.metodologiaCard}>
+            <img src={entregaImg} alt="Ícone entrega" className={styles.metodologiaIcon} />
+            <p>Enviamos a peça com toda proteção necessária. Garantimos qualidade no transporte e incluímos instruções para melhor uso e cuidado futuro.</p>
+          </div>
+        </div>
+      </section>
+
       {/* SEÇÃO 4 : VANTAGENS */}
       <section className={styles.containerVantagens}>
-
+       <div className={styles.pinkSection}>
         <p className={styles.tituloVantagens}>
           Vantagens da Impressão 3D
         </p>
@@ -137,7 +144,7 @@ function Impressao3D() {
           </div>
 
         </div>
-
+      </div>
       </section>
     </>
   );
