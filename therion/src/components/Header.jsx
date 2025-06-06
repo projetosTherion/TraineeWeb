f9679f5010eed7a36238d816a72fb4917942874d
Gu1-Fr4nc0 <guilhermefranco.2003@alunos.utfpr.edu.br>
2025-06-06 14:55:05 -0300

    Header

--- therion/src/components/Header.jsx
+++ therion/src/components/Header.jsx
@@ -1,32 +1,40 @@
 import { Link } from 'react-router-dom';
 import LogoBranca from '../assets/LogoBranca.png';
+import './Header.css';
 
 function Header() {
   return (
-    <header style={{ background: '#FF5991', padding: '10px 10px', color: '#fff' }}>
-      <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-        
-        {/* Logo no canto esquerdo */}
-        <div style={{ flex: '1', textAlign: 'left' }}>
+    <header className="header">
+      <div className="header-container">
+        {/* Logo */}
+        <Link to="/" className="logo-link">
           <img 
             src={LogoBranca} 
-            alt="Logo" 
-            style={{ height: '47px' }} 
+            alt="Logo Therion" 
+            className="header-logo" 
           />
-        </div>
+        </Link>
 
-        {/* Navegação centralizada */}
-        <nav style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)'}}>
-          <ul style={{ listStyle: 'none', display: 'flex', gap: '30px', margin: 0, padding: 0, fontSize: '27px'}}>
-            <li><Link to="/" style={{ color: '#fff' }}>Home</Link></li>
-            <li><Link to="/sobre" style={{ color: '#fff' }}>Sobre Nós</Link></li>
-            <li><Link to="/projetos" style={{ color: '#fff' }}>Projetos</Link></li>
-            <li><Link to="/impressao3d" style={{ color: '#fff' }}>Impressão 3D</Link></li>
-            <li><Link to="/contato" style={{ color: '#fff' }}>Contato</Link></li>
+        {/* Menu de navegação */}
+        <nav className="nav-menu">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sobre" className="nav-link">Sobre Nós</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/projetos" className="nav-link">Projetos</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/impressao3d" className="nav-link">Impressão 3D</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contato" className="nav-link">Contato</Link>
+            </li>
           </ul>
         </nav>
-
-
       </div>
     </header>
   );
